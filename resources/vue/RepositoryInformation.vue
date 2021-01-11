a590a7253624f62d3b0811aeaae4ff60adb40d34
DJYoko <yokochan.recurrent@gmail.com>
2021-01-11 22:24:44 +0900

    add information element as Modal

---
+++ resources/vue/RepositoryInformation.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li>
+      author:
+      <a target="_blank" href="https://github.com/djyoko"
+        >dj_yoko (https://github.com/djyoko)
+      </a>
+    </li>
+    <li>
+      repository:
+      <a target="_blank" href="https://github.com/DJYoko/vueThreeDemo"
+        >https://github.com/DJYoko/vueThreeDemo</a
+      >
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'RepositoryInformation',
+}
+</script>
