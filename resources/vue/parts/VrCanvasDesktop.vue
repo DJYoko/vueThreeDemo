364cac00079acfdedc3075d1c51e0115ed923213
DJYoko <yokochan.recurrent@gmail.com>
2021-01-26 00:14:53 +0900

    move snowing & background image to mixin

--- resources/vue/parts/VrCanvasDesktop.vue
+++ resources/vue/parts/VrCanvasDesktop.vue
@@ -3,12 +3,15 @@
 </template>
 
 <script>
+import snowing from '../mixins/snowing'
+import backgroundSphere from '../mixins/backgroundSphere'
 import * as THREE from 'three'
 
 const OrbitControls = require('three-orbit-controls')(THREE)
 
 export default {
   name: 'VrCanvas',
+  mixins: [snowing, backgroundSphere],
   data() {
     const renderer = new THREE.WebGLRenderer()
     const camera = null
@@ -66,7 +69,7 @@ export default {
 
     // this.addCube()
     this.snowing()
-    this.addImage()
+    this.addBackgroundSphere()
 
     // start motion
     this._tick()
@@ -77,23 +80,7 @@ export default {
       this.renderer.render(this.scene, this.camera)
       this.orbitControls.update()
     },
-    addImage() {
-      const texture = new THREE.TextureLoader().load(
-        '../img/6860371067_fe759ef565_h.jpg',
-        (tex) => {
-          const geometry = new THREE.SphereGeometry(5, 24, 18)
-          geometry.scale(-1, 1, 1)
 
-          const material = new THREE.MeshBasicMaterial({ map: texture })
-
-          const sphere = new THREE.Mesh(geometry, material)
-          const sphereWidth = 1
-          const sphereHeight = 1
-          sphere.scale.set(sphereWidth, sphereHeight, 1)
-          this.scene.add(sphere)
-        }
-      )
-    },
     setOrbitControls() {
       const htmlelm = this.$refs.elementContainer
       this.orbitControls = new OrbitControls(this.camera, htmlelm)
@@ -102,49 +89,6 @@ export default {
       this.orbitControls.enableZoom = false
       this.orbitControls.maxPolarAngle = 2.6
       this.orbitControls.minPolarAngle = 0.5
-    },
-    snowing() {
-      setInterval(() => {
-        this._addSnow()
-      }, 200)
-    },
-    _addSnow() {
-      // add object
-      const size = 0.05 * Math.random()
-
-      const fallingAreaRange = 4
-      const initialX = Math.random() * fallingAreaRange - fallingAreaRange / 2
-      const initialZ = Math.random() * fallingAreaRange - fallingAreaRange / 2
-
-      const geo = new THREE.SphereGeometry(size, 10, 10)
-      const mat = new THREE.MeshLambertMaterial({ color: 0xffffff })
-      const mesh = new THREE.Mesh(geo, mat)
-      const position = { x: initialX, y: 4, z: initialZ }
-      mesh.position.set(position.x, position.y, position.z)
-
-      this.scene.add(mesh)
-      const fallSpeed = 30
-      const verticalMove = 0.01
-
-      const falling = setInterval(() => {
-        // remove snow when it touches ground
-        if (positionY < 0) {
-          clearInterval(falling)
-          this.scene.remove(mesh)
-          return false
-        }
-
-        const meshPosition = mesh.position
-        const positionXdiff =
-          Math.random() > 0.5 ? verticalMove : -1 * verticalMove
-        const positionZdiff =
-          Math.random() > 0.5 ? verticalMove : -1 * verticalMove
-        const positionX = meshPosition.x + positionXdiff
-        const positionZ = meshPosition.z + positionZdiff
-        const positionY = meshPosition.y - fallSpeed / 400
-
-        meshPosition.set(positionX, positionY, positionZ)
-      }, 1000 / fallSpeed)
     },
   },
 }
--- resources/vue/parts/VrCanvasMobile.vue
+++ resources/vue/parts/VrCanvasMobile.vue
@@ -3,12 +3,15 @@
 </template>
 
 <script>
+import snowing from '../mixins/snowing'
+import backgroundSphere from '../mixins/backgroundSphere'
 import * as THREE from 'three'
 const StereoEffect = require('three-stereo-effect')(THREE)
 const DeviceOrientationControls = require('three-device-orientation')
 
 export default {
   name: 'VrCanvas',
+  mixins: [snowing, backgroundSphere],
   data() {
     const renderer = new THREE.WebGLRenderer()
     const stereoEffect = null
@@ -67,10 +70,9 @@ export default {
     this.light.position.set(2, 2, 2)
     this.scene.add(this.light)
 
-    // this.addCube()
     this.snowing()
     this.setStereoEffect()
-    this.addImage()
+    this.addBackgroundSphere()
 
     // start motion
     this._tick()
@@ -157,49 +159,6 @@ export default {
         this.setOrientationControls,
         true
       )
-    },
-    snowing() {
-      setInterval(() => {
-        this._addSnow()
-      }, 200)
-    },
-    _addSnow() {
-      // add object
-      const size = 0.05 * Math.random()
-
-      const fallingAreaRange = 4
-      const initialX = Math.random() * fallingAreaRange - fallingAreaRange / 2
-      const initialZ = Math.random() * fallingAreaRange - fallingAreaRange / 2
-
-      const geo = new THREE.SphereGeometry(size, 10, 10)
-      const mat = new THREE.MeshLambertMaterial({ color: 0xffffff })
-      const mesh = new THREE.Mesh(geo, mat)
-      const position = { x: initialX, y: 4, z: initialZ }
-      mesh.position.set(position.x, position.y, position.z)
-
-      this.scene.add(mesh)
-      const fallSpeed = 30
-      const verticalMove = 0.01
-
-      const falling = setInterval(() => {
-        // remove snow when it touches ground
-        if (positionY < 0) {
-          clearInterval(falling)
-          this.scene.remove(mesh)
-          return false
-        }
-
-        const meshPosition = mesh.position
-        const positionXdiff =
-          Math.random() > 0.5 ? verticalMove : -1 * verticalMove
-        const positionZdiff =
-          Math.random() > 0.5 ? verticalMove : -1 * verticalMove
-        const positionX = meshPosition.x + positionXdiff
-        const positionZ = meshPosition.z + positionZdiff
-        const positionY = meshPosition.y - fallSpeed / 400
-
-        meshPosition.set(positionX, positionY, positionZ)
-      }, 1000 / fallSpeed)
     },
   },
 }
